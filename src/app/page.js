f7c144962df8008c43ff6737448ce72b285e7214
fadhil aly <alyfadhil24@gmail.com>
2024-06-21 14:12:58 +0700

    update css using tailwind

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import data from "./data";
-import styles from "./page.module.css";
 
 export default function Home() {
   const [result, setResult] = useState("");
@@ -20,22 +19,26 @@ export default function Home() {
   };
 
   return (
-    <div className={styles.container}>
-      <div className={styles.box}>
-        <div className={styles.formInput}>
+    <div className="flex items-center justify-center min-h-screen bg-center bg-cover bg-custom-image">
+      <div className="flex flex-col items-center justify-center w-full max-w-lg p-6 bg-white bg-opacity-50 border-2 border-white rounded-lg">
+        <div className="flex flex-col items-center justify-center w-full gap-4 mb-8">
           <input
             type="text"
             id="input"
             name="input"
             placeholder="Cek khodam kamu disini yuk!"
-            className={styles.input}
+            className="w-full h-12 text-2xl text-center border-2 border-transparent rounded-full focus:outline-none focus:bg-gray-800 focus:text-white focus:border-gray-300"
           />
-          <button id="cek" onClick={submitValue} className={styles.button}>
+          <button
+            id="cek"
+            onClick={submitValue}
+            className="w-20 h-12 text-lg font-bold text-center rounded-full cursor-pointer border-2 border-gray-600 hover:bg-gray-200"
+          >
             Cek
           </button>
         </div>
-        <div className={styles.result}>
-          <p>{result}</p>
+        <div className="flex items-center justify-center w-full">
+          <p className="text-2xl font-bold text-black">{result}</p>
         </div>
       </div>
     </div>
